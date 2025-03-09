31f8d70588d57f4d111dd9e06fe1fa966d2e5aa2
muzilka <norinessa2518@gmail.com>
2025-03-09 13:13:40 +0500

    Делаем мультиплеер

---
+++ docs/bk/v2/beta-v1/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1741507893,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/kuzya-sheet0.png",
+		"images/fon.png",
+		"images/game-sheet0.png",
+		"images/down-sheet0.png",
+		"images/righty-sheet0.png",
+		"images/lefty-sheet0.png",
+		"images/up-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
